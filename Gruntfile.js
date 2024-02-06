e39a04b3616e9c5e9ccabb0ac40fa9ae3dc0e4db
Coridyn Fitzgerald-Hood <coridyn@widgetworks.com.au>
2024-02-06 17:13:18 +1000

    feat(package): update dependencies; remove jshint [sc-10373]

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,16 +11,6 @@
 module.exports = function (grunt) {
 	// Project configuration.
 	grunt.initConfig({
-		jshint: {
-			all: [
-				'Gruntfile.js',
-				'lib/*.js',
-				'test/*.js'
-			],
-			options: {
-				jshintrc: '.jshintrc'
-			}
-		},
 		mochaTest: {
 			test: {
 				options: {
@@ -60,12 +50,10 @@ module.exports = function (grunt) {
 		}
 	})
 
-	grunt.loadNpmTasks('grunt-contrib-jshint')
 	grunt.loadNpmTasks('grunt-mocha-test')
 	grunt.loadNpmTasks('grunt-contrib-clean')
 	grunt.loadNpmTasks('grunt-contrib-copy')
 
 	// By default, lint and run all tests.
-	grunt.registerTask('lint', 'jshint')
 	grunt.registerTask('default', ['clean', 'copy', 'mochaTest:test', 'clean', 'copy', 'mochaTest:testDefaultOptions', 'clean', 'copy', 'mochaTest:testBasePath', 'clean', 'copy', 'mochaTest:testDefaultOptionsAndBasePath'])
 }
